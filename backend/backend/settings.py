091d2914453783676dc4022635ce4b41479aab9f
Jacek Palczewski <jpalczewski@gmail.com>
2022-02-27 12:00:46 +0100

    [BUG][4] fix crlf errors, add passing django secret via env

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import dj_database_url
 import environ
 
 env = environ.Env(
@@ -27,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8%hocl_g^j1+l8jttj^d1)5njrrys)495=%q^c2t^-%v9sc$ed'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
