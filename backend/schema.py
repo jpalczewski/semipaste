5c871c836d789507ceebefb127d8889733cad72d
Geroid64 <gerodin64@gmail.com>
2022-03-10 20:16:32 +0100

    [BE][7]Added relay and cleaned code in schema

--- backend/schema.py
+++ backend/schema.py
@@ -3,25 +3,23 @@ from pastes.models import PasteBin
 from graphene_django.types import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 from graphene import Node
+from graphene import relay
 from graphene_django.debug import DjangoDebug
 
+
 class PasteBinNode(DjangoObjectType):
+    id = graphene.ID(source='pk', required=True)
     class Meta:
         model = PasteBin
-        interfaces = (Node,)
-        fields = "__all__"
-        filter_fields = ["title"]
+        filter_fields = ['title','id']
+        interfaces = (relay.Node,)
+    #rowid = graphene.String()
+        #fields = "__all__"
 
-class Query(object):
-    PasteBinQuery = Node.Field(PasteBinNode)
+class Query(graphene.ObjectType):
     AllPasteBin = DjangoFilterConnectionField(PasteBinNode)
-
-
-
-class Query2(
-    Query,
-    graphene.ObjectType,
-):
+    node = graphene.relay.Node.Field(PasteBinNode)
     debug = graphene.Field(DjangoDebug, name="_debug")
 
-schema = graphene.Schema(query=Query2)
+
+schema = graphene.Schema(query=Query)
