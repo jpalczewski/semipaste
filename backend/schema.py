b30832cd32f1614dde58f49c265aa64b9cdf8025
Geroid64 <gerodin64@gmail.com>
2022-03-11 12:26:59 +0100

    [BE][17]Add userNode

--- backend/schema.py
+++ backend/schema.py
@@ -1,11 +1,12 @@
 import graphene
-from pastes.models import PasteBin
 from graphene_django.types import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 from graphene import Node
 from graphene import relay
 from graphene_django.debug import DjangoDebug
 
+from pastes.models import PasteBin
+from users.models import User
 
 class PasteBinNode(DjangoObjectType):
     id = graphene.ID(source='pk', required=True)
@@ -16,9 +17,17 @@ class PasteBinNode(DjangoObjectType):
     #rowid = graphene.String()
         #fields = "__all__"
 
+class UserNode(DjangoObjectType):
+    id = graphene.ID(source='pk', required=True)
+    class Meta:
+        model = User
+        filter_fields = ['id']
+        interfaces = (relay.Node,)
+
 class Query(graphene.ObjectType):
     AllPasteBin = DjangoFilterConnectionField(PasteBinNode)
-    node = graphene.relay.Node.Field(PasteBinNode)
+    AllUsers = DjangoFilterConnectionField(UserNode)
+    node = graphene.relay.Node.Field()
     debug = graphene.Field(DjangoDebug, name="_debug")
 
 
