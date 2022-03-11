f924224e2dd7989e3602c5cabe9e2f1785e1b4a9
Jacek Palczewski <jpalczewski@gmail.com>
2022-03-11 13:33:08 +0100

    [BE][7] add pastebin form, mutation and bump graphene-django version

--- backend/schema.py
+++ backend/schema.py
@@ -1,20 +1,35 @@
 import graphene
 from pastes.models import PasteBin
+from pastes.forms import AddPasteBinForm
 from graphene_django.types import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 from graphene import Node
 from graphene import relay
 from graphene_django.debug import DjangoDebug
+from graphene_django.forms.mutation import DjangoModelFormMutation
 
 
 class PasteBinNode(DjangoObjectType):
     id = graphene.ID(source='pk', required=True)
+
     class Meta:
         model = PasteBin
-        filter_fields = ['title','id']
+        filter_fields = ['title', 'id']
         interfaces = (relay.Node,)
-    #rowid = graphene.String()
-        #fields = "__all__"
+    # rowid = graphene.String()
+    # fields = "__all__"
+
+
+class PasteBinMutation(DjangoModelFormMutation):
+    class Meta:
+        form_class = AddPasteBinForm
+        exclude_fields = ('id',)
+
+
+class Mutation(graphene.ObjectType):
+    add_paste_bin = PasteBinMutation.Field()
+    pass
+
 
 class Query(graphene.ObjectType):
     AllPasteBin = DjangoFilterConnectionField(PasteBinNode)
@@ -22,4 +37,4 @@ class Query(graphene.ObjectType):
     debug = graphene.Field(DjangoDebug, name="_debug")
 
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
