191689af33ef9dfd2d440b387b325a14a0e549b3
Jacek Palczewski <jpalczewski@gmail.com>
2022-03-20 15:12:43 +0100

    [BUG] fix rebasing mistakes

--- backend/pastes/queries.py
+++ backend/pastes/queries.py
@@ -1,9 +1,12 @@
+# 3rd-Party
 import graphene
 from graphene import relay
 from graphene_django.filter import DjangoFilterConnectionField
 
+# Local
 from .mutations import PasteBinNode
 
+
 class PasteBinQuery(graphene.ObjectType):
-   all_paste_bin = DjangoFilterConnectionField(PasteBinNode)
-   paste_bin = relay.Node.Field(PasteBinNode)
+    all_paste_bin = DjangoFilterConnectionField(PasteBinNode)
+    paste_bin = relay.Node.Field(PasteBinNode)
