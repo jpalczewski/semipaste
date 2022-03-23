41e8ee3d0c87f52df296e7183aa1a7e03807c328
michals-lab <59738119+michals-lab@users.noreply.github.com>
2022-03-23 19:36:30 +0100

    update queries

--- frontend/src/Query/editUser.js
+++ frontend/src/Query/editUser.js
@@ -1,7 +1,7 @@
 import graphql from "babel-plugin-relay/macro";
 
 export const editUser = graphql`
-  mutation editUserMutation($input: EditInput) {
+  mutation editUserMutation($input: variables!) {
     editUser(input: $input) {
       id
       username
