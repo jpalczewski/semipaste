b837a32aeb7e270fd294c6e1118c202cef7a7697
Jacek Palczewski <jpalczewski@gmail.com>
2022-03-24 11:55:30 +0100

    [FE][15] Lot of fixes and new bugs introduced

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { Popover } from "react-bootstrap";
 import { RelayEnvironmentProvider } from "react-relay";
 import "./App.css";
+import { Popular } from "./components/PopularScreen";
 import { Routerr } from "./navigation/Router";
 import RelayEnvironment from "./RelayEnvironment";
 
 const App = () => {
   return (
     <RelayEnvironmentProvider environment={RelayEnvironment}>
+      <Suspense fallback={<h1>trwa formatowanie dysku twardego</h1>} >
       <Routerr />
+      </Suspense>
     </RelayEnvironmentProvider>
   );
 };
