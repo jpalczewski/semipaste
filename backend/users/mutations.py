e7b69b84e1f56b60b5cfcfedffae19badb65703c
Geroid64 <gerodin64@gmail.com>
2022-03-26 19:41:53 +0100

    [BE][34] Add required ID user

--- backend/users/mutations.py
+++ backend/users/mutations.py
@@ -38,15 +38,16 @@ class EditUser(graphene.Mutation):
     ok = graphene.Boolean()
 
     class Arguments:
-        id = graphene.ID()
+        id = graphene.ID(required=True)
         username = graphene.String()
         first_name = graphene.String()
         last_name = graphene.String()
         email = graphene.String()
         description = graphene.String()
 
-    def mutate(cls, info, **kwargs):  # type: ignore
-        user = User.objects.get(pk=kwargs["id"])
+    def mutate(cls, info, id: graphene.ID, **kwargs):  # type: ignore
+        # type: ignore
+        user = User.objects.get(pk=id)
         for attr in kwargs.keys():
             value = kwargs.get(attr, getattr(user, attr))
             if attr == 'description':
@@ -61,11 +62,11 @@ class EditUserDescription(graphene.Mutation):
     ok = graphene.Boolean()
 
     class Arguments:
-        id = graphene.ID()
+        id = graphene.ID(required=True)
         description = graphene.String()
 
-    def mutate(cls, info, **kwargs):  # type: ignore
-        user = User.objects.get(pk=kwargs["id"])
+    def mutate(cls, info, id: graphene.ID, **kwargs):  # type: ignore
+        user = User.objects.get(pk=id)
         user.description = strip_tags(
             escape(kwargs.get('description', user.description))
         )
@@ -77,10 +78,10 @@ class DeleteUser(graphene.Mutation):
     ok = graphene.Boolean()
 
     class Arguments:
-        id = graphene.ID()
+        id = graphene.ID(required=True)
 
-    def mutate(cls, info, **kwargs):  # type: ignore
-        user = User.objects.get(pk=kwargs["id"])
+    def mutate(cls, info, id: graphene.ID, **kwargs):  # type: ignore
+        user = User.objects.get(pk=id)
         user.delete()
         return cls(ok=True)
 
