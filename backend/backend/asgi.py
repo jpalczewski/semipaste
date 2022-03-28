151b89732c4ac468d22b5e6ad41b4b4e4ec04162
Jacek Palczewski <jpalczewski@gmail.com>
2022-03-28 15:03:37 +0200

    Revert

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -15,5 +15,6 @@ import os
 from configurations.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
 
 application = get_asgi_application()
