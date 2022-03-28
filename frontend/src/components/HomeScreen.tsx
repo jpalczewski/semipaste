00ba0d120964ca11d6d947dda23ab3d25333fe7d
michals-lab <59738119+michals-lab@users.noreply.github.com>
2022-03-28 13:34:54 +0200

    create user

--- frontend/src/components/HomeScreen.tsx
+++ frontend/src/components/HomeScreen.tsx
@@ -1,13 +1,22 @@
-import React from "react";
-import { Wrapper } from "../styles/Components.style";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+import { Wrapper, Footer } from "../styles/Components.style";
 
 import { LoginScreen } from "./user/LoginScreen";
+import { RegistrationScreen } from "./user/RegistrationScreen";
 
 export const Home = () => {
+  const [vis, setVis] = useState(false);
   return (
     <Wrapper>
       <p>Strona domowa</p>
-      <LoginScreen />
+      {!vis ? <LoginScreen /> : <RegistrationScreen />}
+      <Footer>
+        {!vis ? "Nie posiadasz konta?  " : "Posiadasz już konto?  "}
+        <Button variant="success" onClick={() => setVis(!vis)}>
+          {!vis ? "Zarejestruj się" : "Zaloguj się"}
+        </Button>
+      </Footer>
     </Wrapper>
   );
 };
