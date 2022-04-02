7a07d0385c79eb4bdb2a8bc5afb401ddd9671581
Jacek Palczewski <jpalczewski@gmail.com>
2022-04-02 10:54:51 +0200

    [BE][37] Add a new time range and deprecation note

--- backend/pastes/queries.py
+++ backend/pastes/queries.py
@@ -8,7 +8,10 @@ from .mutations import ActivePasteBin, ExpiredPasteBin, PasteBinNode
 
 
 class PasteBinQuery(graphene.ObjectType):
-    all_paste_bin = DjangoFilterConnectionField(PasteBinNode)
+    all_paste_bin = DjangoFilterConnectionField(
+        PasteBinNode,
+        deprecation_reason="It will be soon available only for " "superusers",
+    )
     active_paste_bin = DjangoFilterConnectionField(ActivePasteBin)
     expired_paste_bin = DjangoFilterConnectionField(ExpiredPasteBin)
     paste_bin = relay.Node.Field(PasteBinNode)
