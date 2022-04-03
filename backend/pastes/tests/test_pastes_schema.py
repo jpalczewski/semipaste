859ce80bd0e1b5d9291adc3de16d94e77862619f
adamzal <adam.zalewski1999@o2.pl>
2022-04-03 07:35:56 +0200

    add test 13

--- backend/pastes/tests/test_pastes_schema.py
+++ backend/pastes/tests/test_pastes_schema.py
@@ -170,3 +170,12 @@ class TestSchema(TestCase):
         query_result = self.client.execute(expireafter_query)
         self.assertDictEqual({"data": {"addPasteBin": {"ok": True}}}, mutation_result)
         self.assertDictEqual({"data": {"allPasteBin": {"edges": [{"node": {"expireAfter": "WEEK"}}]}}}, query_result)
+
+    def test_13_showPasteBin_expireAfter_MONTH(self) -> None:
+        expireafter_query = """query{ allPasteBin { edges { node { expireAfter}}}}"""
+        mutation_result = self.client.execute(
+            """mutation{addPasteBin(title: "Title test", text: "Paste text test", exposure: true, expireAfter: "MONTH") {ok}}""",
+            context=self.user)
+        query_result = self.client.execute(expireafter_query)
+        self.assertDictEqual({"data": {"addPasteBin": {"ok": True}}}, mutation_result)
+        self.assertDictEqual({"data": {"allPasteBin": {"edges": [{"node": {"expireAfter": "MONTH"}}]}}}, query_result)
