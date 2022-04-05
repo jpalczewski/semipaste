6942f258f0f0c3adafa79baed8286b935195ae58
adamzal <adam.zalewski1999@o2.pl>
2022-04-06 00:00:19 +0200

    update test 01

--- backend/pastes/tests/test_pastes_schema.py
+++ backend/pastes/tests/test_pastes_schema.py
@@ -4,37 +4,20 @@ from django.test import TestCase
 
 # 3rd-Party
 import graphene
+from graphene_django.utils.testing import GraphQLTestCase
 from graphene.test import Client
 
 # Project
 from schema import Mutation, Query
 
 
-class TestSchema(TestCase):
+class TestSchema(GraphQLTestCase):
     def setUp(self) -> None:
         class Object:
             user = get_user_model().objects.create()
 
         self.user = Object
 
-        self.query = """
-            query{
-              allPasteBin {
-                edges {
-                  node {
-                    id
-                    title
-                    text
-                    exposure
-                    expireAfter
-                    author{
-                        id
-                        }
-                  }
-                }
-              }
-            }
-        """
         self.mutation = """
         mutation{
           addPasteBin(
@@ -50,8 +33,9 @@ class TestSchema(TestCase):
         self.client = Client(graphene.Schema(query=Query, mutation=Mutation))
 
     def test_01_showAllPasteBins_beforeAddMutation(self) -> None:
-        result = self.client.execute(self.query)
-        self.assertDictEqual({"data": {"allPasteBin": {"edges": []}}}, result)
+        query ="""query{allPasteBin {edges {node {id title text exposure expireAfter author{id}}}}}"""
+        response = self.client.execute(query)
+        self.assertEqual(response["data"]["allPasteBin"]["edges"],[])
 
     def test_02_addPasteBin_mutation(self) -> None:
         mutation_result = self.client.execute(self.mutation, context=self.user)
