166ee5e142fb839a67dfa490f375f32123e733eb
adamzal <adam.zalewski1999@o2.pl>
2022-04-07 03:56:58 +0200

    add test 03 in users schema test

--- backend/users/tests/test_users_schema.py
+++ backend/users/tests/test_users_schema.py
@@ -32,3 +32,17 @@ class TestSchema(TestCase):
 
         mutation_result = self.client.execute(mutation, variable_values=variables)
         self.assertEqual(mutation_result["data"]["addUser"]["ok"], True)
+
+    def test_03_showUser_id_1(self) -> None:
+        id_query = """query{allUsers{edges{node{id}}}}"""
+        mutation = """mutation($confirmPassword: String! $email: String! $password: String! $username: String!){
+        addUser(confirmPassword: $confirmPassword, email: $email, password: $password, username: $username){ok }} """
+        example_user = UserFactory()
+        variables = {"confirmPassword": example_user.password,
+                     "email": example_user.email,
+                     "password": example_user.password,
+                     "username": "Test03"}
+        mutation_result = self.client.execute(mutation, variable_values=variables)
+        query_result = self.client.execute(id_query)
+        self.assertEqual(mutation_result["data"]["addUser"]["ok"], True)
+        self.assertEqual(query_result["data"]["allUsers"]["edges"][0]["node"]["id"], '21')
