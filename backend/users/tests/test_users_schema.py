da2eb11fc03e5fca3fa88882c8e7a8018f0e1188
adamzal <adam.zalewski1999@o2.pl>
2022-04-07 03:51:34 +0200

    add test 02 in users schema test

--- backend/users/tests/test_users_schema.py
+++ backend/users/tests/test_users_schema.py
@@ -7,6 +7,7 @@ from graphene.test import Client
 
 # Project
 from schema import Mutation, Query
+from ..factories import UserFactory
 
 
 class TestSchema(TestCase):
@@ -19,3 +20,15 @@ class TestSchema(TestCase):
         isActive}}}} """
         query_result = self.client.execute(query)
         self.assertEqual(query_result["data"]["allUsers"]["edges"], [])
+
+    def test_02_addUser_mutation(self) -> None:
+        mutation = """mutation($confirmPassword: String! $email: String! $password: String! $username: String!){
+        addUser(confirmPassword: $confirmPassword, email: $email, password: $password, username: $username){ok }} """
+        example_user = UserFactory()
+        variables = {"confirmPassword": example_user.password,
+                     "email": example_user.email,
+                     "password": example_user.password,
+                     "username": "Test02"}
+
+        mutation_result = self.client.execute(mutation, variable_values=variables)
+        self.assertEqual(mutation_result["data"]["addUser"]["ok"], True)
