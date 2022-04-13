326037447a482500feba8773702c5920fe46e647
michals-lab <59738119+michals-lab@users.noreply.github.com>
2022-04-13 21:47:24 +0200

    custom hook localstorage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,10 @@
 import React, { Suspense } from "react";
-
 import { RelayEnvironmentProvider } from "react-relay";
-import "./App.css";
-
-import { Routerr } from "./navigation/Router";
 import RelayEnvironment from "./RelayEnvironment";
+import "./App.css";
+import { Routerr } from "./navigation/Router";
 
-const App = () => {
+const App: React.FC = () => {
   return (
     <RelayEnvironmentProvider environment={RelayEnvironment}>
       <Suspense fallback={<h1>trwa formatowanie dysku twardego</h1>}>
