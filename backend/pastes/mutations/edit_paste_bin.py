9413c1d9c9fffef26cc5163789317f09781a01df
Geroid64 <gerodin64@gmail.com>
2022-04-27 17:06:53 +0200

    [BE][64] continue Edit tag

--- backend/pastes/mutations/edit_paste_bin.py
+++ backend/pastes/mutations/edit_paste_bin.py
@@ -5,7 +5,7 @@ import graphene
 
 # Project
 from backend.mixins import ResultMixin
-from pastes.models import PasteBin
+from pastes.models import MTMTags, PasteBin, PasteTag
 
 
 class EditPasteBin(ResultMixin, graphene.Mutation):
@@ -14,6 +14,7 @@ class EditPasteBin(ResultMixin, graphene.Mutation):
         title = graphene.String()
         text = graphene.String()
         exposure = graphene.Boolean()
+        tags = graphene.List(graphene.String)
 
     @staticmethod
     def mutate(cls, info, id, **kwargs):  # type: ignore
@@ -22,16 +23,16 @@ class EditPasteBin(ResultMixin, graphene.Mutation):
             paste = PasteBin.objects.get(pk=id)
         except paste.DoesNotExist:
             return EditPasteBin(ok=False, error="No such paste")
-
         if user.id != paste.author_id or user.is_superuser:
             return EditPasteBin(ok=False, error="Not your paste")
+        MTMTags.objects.filter(paste_id=paste.id).delete()
         for attr, val in kwargs.items():
             if val != '':
-                if attr == 'text':
-                    setattr(paste, attr, val)
-                elif attr == 'title':
-                    setattr(paste, attr, val)
-                elif attr == 'exposure':
+                if attr == 'tags':
+                    for tag in val:
+                        tag_get = PasteTag.objects.get_or_create(tag_name=tag)
+                        MTMTags(paste_id=paste.id, tag_id=tag_get.id).save()
+                else:
                     setattr(paste, attr, val)
         paste.save()
         return EditPasteBin(ok=True, error="Paste changed")
