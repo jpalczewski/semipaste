7a4fd7539a81e31d4391064b44a2a936fd8cd7b1
Geroid64 <gerodin64@gmail.com>
2022-05-05 11:45:32 +0200

    [BE][64] Add delete tag

--- backend/pastes/mutations/__init__.py
+++ backend/pastes/mutations/__init__.py
@@ -7,6 +7,7 @@ from .add_paste_bin import AddPasteBin
 from .add_tag import AddTag
 from .delete_attachement import DeleteAttachment
 from .delete_paste_bin import DeletePasteBin
+from .delete_tag import DeleteTag
 from .edit_paste_bin import EditPasteBin
 from .highlight import HighlightPasteBin, HighlightPreview
 from .rating_paste_bin import IsPasteBinRated, RatingPasteBin, RatingPasteBinID
@@ -22,6 +23,7 @@ class PasteBinMutation(graphene.ObjectType):
     add_attachment = AddAttachment.Field(description="Add an attachment to a paste")
     delete_attachment = DeleteAttachment.Field(description="Delete an attachment")
     add_tag = AddTag.Field(description="manually add a new tag")
+    delete_tag = DeleteTag.Field(description="manually delete a tag")
     edit_paste = EditPasteBin.Field()
     rate_paste_bin = RatingPasteBin.Field()
     rate_paste_bin_id = RatingPasteBinID.Field()
