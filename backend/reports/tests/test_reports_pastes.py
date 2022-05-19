31983cf4b93b743fc2f4f3ce3dfa1fab23c09fba
adamzal <adam.zalewski1999@o2.pl>
2022-05-19 07:28:50 +0200

    reports tests

---
+++ backend/reports/tests/test_reports_pastes.py
@@ -0,0 +1,260 @@
+# Django
+from django.contrib.auth.models import AnonymousUser
+
+# 3rd-Party
+import graphene
+from graphene.test import Client
+from graphene_django.utils.testing import GraphQLTestCase
+
+# Project
+from schema import Mutation, Query
+from users.factories import UserFactory
+
+# Local
+from pastes.factories import PasteBinFactory
+
+
+class TestSchema(GraphQLTestCase):
+    def setUp(self) -> None:
+        class User:
+            user = UserFactory()
+
+        class User2:
+            user = AnonymousUser()
+
+        self.pasteBin = PasteBinFactory()
+        self.user = User
+        self.anonymousUser = User2
+        self.client = Client(graphene.Schema(query=Query, mutation=Mutation))
+
+    def test_01_showPasteReports_beforeReportPaste(self) -> None:
+        query = """query{
+                        pasteReports {
+                            edges {
+                                node {
+                                    id
+                                    reason
+                                    paste{
+                                        id
+                                        title
+                                        text
+                                        visible
+                                        expireAfter
+                                        author{
+                                            id
+                                        }
+                                        language                                        
+                                      }                                    
+                                    author{
+                                        id
+                                    }
+                                }
+                            }
+                        }
+                    }"""
+
+        query_result = self.client.execute(query, context=self.user)
+
+        self.assertEqual(query_result["data"]["pasteReports"]["edges"], [])
+
+    def test_02_reportPaste_mutation(self) -> None:
+        mutation = """mutation(
+                      $pid: ID!
+                      $reason: String!){
+                      reportPaste(
+                        input:{
+                            pid: $pid,
+                            reason: $reason})
+                        {
+                          ok
+                          error
+                          errorCode
+                          clientMutationId
+                        }
+                    }"""
+
+        variables = {
+            "pid": 35,
+            "reason": "Reason test 02"
+        }
+
+        mutation_result = self.client.execute(mutation, variable_values=variables, context=self.user)
+
+        self.assertEqual(mutation_result["data"]["reportPaste"]["ok"], True)
+        self.assertEqual(mutation_result["data"]["reportPaste"]["error"], None)
+        self.assertEqual(mutation_result["data"]["reportPaste"]["errorCode"], "POSSIBLE_FAILURE")
+        self.assertEqual(mutation_result["data"]["reportPaste"]["clientMutationId"], None)
+
+    def test_03_reportPaste_mutation_userNotLogged(self) -> None:
+        mutation = """mutation(
+                      $pid: ID!
+                      $reason: String!){
+                      reportPaste(
+                        input:{
+                            pid: $pid,
+                            reason: $reason})
+                        {
+                          ok
+                          error
+                          errorCode
+                          clientMutationId
+                        }
+                    }"""
+
+        variables = {
+            "pid": 36,
+            "reason": "Reason test 03"
+        }
+
+        mutation_result = self.client.execute(mutation, variable_values=variables, context=self.anonymousUser)
+
+        self.assertEqual(mutation_result["data"]["reportPaste"]["ok"], False)
+        self.assertEqual(mutation_result["data"]["reportPaste"]["error"], "You need to be logged in")
+        self.assertEqual(mutation_result["data"]["reportPaste"]["errorCode"], "POSSIBLE_FAILURE")
+        self.assertEqual(mutation_result["data"]["reportPaste"]["clientMutationId"], None)
+
+    def test_04_reportPaste_mutation_pasteNotFound(self) -> None:
+        mutation = """mutation(
+                      $pid: ID!
+                      $reason: String!){
+                      reportPaste(
+                        input:{
+                            pid: $pid,
+                            reason: $reason})
+                        {
+                          ok
+                          error
+                          errorCode
+                          clientMutationId
+                        }
+                    }"""
+
+        variables = {
+            "pid": 999,
+            "reason": "Reason test 04"
+        }
+
+        mutation_result = self.client.execute(mutation, variable_values=variables, context=self.user)
+
+        self.assertEqual(mutation_result["data"]["reportPaste"]["ok"], False)
+        self.assertEqual(mutation_result["data"]["reportPaste"]["error"], "Reported paste not found")
+        self.assertEqual(mutation_result["data"]["reportPaste"]["errorCode"], "POSSIBLE_FAILURE")
+        self.assertEqual(mutation_result["data"]["reportPaste"]["clientMutationId"], None)
+
+    def test_05_reportPaste_correctData(self) -> None:
+        query = """query{
+                    pasteReports {
+                        edges {
+                            node {
+                                id
+                                reason
+                                paste{
+                                    id
+                                    title
+                                    text
+                                    visible
+                                    expireAfter
+                                    author{
+                                        id
+                                    }
+                                    language                                        
+                                  }                                    
+                                author{
+                                    id
+                                }
+                            }
+                        }
+                    }
+                }"""
+
+        mutation = """mutation(
+                      $pid: ID!
+                      $reason: String!){
+                      reportPaste(
+                        input:{
+                            pid: $pid,
+                            reason: $reason})
+                        {
+                          ok
+                          error
+                          errorCode
+                          clientMutationId
+                        }
+                    }"""
+
+        variables = {
+            "pid": 38,
+            "reason": "Reason test 05"
+        }
+
+        self.client.execute(mutation, variable_values=variables, context=self.user)
+        query_result = self.client.execute(query, context=self.user)
+
+        self.assertEqual(query_result["data"]["pasteReports"]["edges"][0]["node"]["reason"], "Reason test 05")
+        self.assertEqual(query_result["data"]["pasteReports"]["edges"][0]["node"]["paste"]["id"], "38")
+        self.assertEqual(query_result["data"]["pasteReports"]["edges"][0]["node"]["paste"]["title"],
+                         self.pasteBin.title)
+        self.assertEqual(query_result["data"]["pasteReports"]["edges"][0]["node"]["paste"]["text"],
+                         self.pasteBin.text)
+        self.assertEqual(query_result["data"]["pasteReports"]["edges"][0]["node"]["paste"]["visible"],
+                         self.pasteBin.visible)
+        self.assertEqual(query_result["data"]["pasteReports"]["edges"][0]["node"]["paste"]["expireAfter"],
+                         self.pasteBin.expire_after)
+        self.assertEqual(query_result["data"]["pasteReports"]["edges"][0]["node"]["paste"]["author"],
+                         self.pasteBin.author)
+        self.assertEqual(query_result["data"]["pasteReports"]["edges"][0]["node"]["paste"]["language"],
+                         self.pasteBin.language)
+        self.assertEqual(query_result["data"]["pasteReports"]["edges"][0]["node"]["author"]["id"], "27")
+
+    def test_06_isNotSuperuser(self) -> None:
+        query = """query{
+                    pasteReports {
+                        edges {
+                            node {
+                                id
+                                reason
+                                paste{
+                                    id
+                                    title
+                                    text
+                                    visible
+                                    expireAfter
+                                    author{
+                                        id
+                                    }
+                                    language                                        
+                                  }                                    
+                                author{
+                                    id
+                                }
+                            }
+                        }
+                    }
+                }"""
+
+        mutation = """mutation(
+                              $pid: ID!
+                              $reason: String!){
+                              reportPaste(
+                                input:{
+                                    pid: $pid,
+                                    reason: $reason})
+                                {
+                                  ok
+                                  error
+                                  errorCode
+                                  clientMutationId
+                                }
+                            }"""
+
+        variables = {
+            "pid": 39,
+            "reason": "Reason test 06"
+        }
+
+        self.client.execute(mutation, variable_values=variables, context=self.user)
+        self.user.user.is_superuser = False
+        query_result = self.client.execute(query, context=self.user)
+
+        self.assertEqual(query_result["data"]["pasteReports"]["edges"], [])
+
